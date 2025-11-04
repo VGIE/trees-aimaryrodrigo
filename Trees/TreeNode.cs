b1776c9cee2ca7336e1fc5080c56224e7a33e0b3
Aimar <aimard8a10@gmail.com>
2025-11-04 17:23:07 +0100

    trees completado

--- Trees/TreeNode.cs
+++ Trees/TreeNode.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Reflection.Metadata;
+using System.Runtime.Serialization.Formatters;
 using Lists;
 
 namespace Trees
@@ -8,12 +10,13 @@ namespace Trees
         private T Value;
 
         //TODO #1: Declare a member variable called "Children" as a list of TreeNode<T> objects
-        TreeNode<T> Children ;
+        List <TreeNode<T>> Children ;
 
         public TreeNode(T value)
         {
             //TODO #2: Initialize member variables/attributes
-            this.Value = value;
+            Children = new List<TreeNode<T>> ();
+            Value = value;
         }
 
         public string ToString(int depth, int index)
@@ -40,39 +43,95 @@ namespace Trees
         public TreeNode<T> Add(T value)
         {
             //TODO #4: Add a new instance of class GenericTreeNode<T> with Value=value. Return the instance we just created
-
-            return null;
+            TreeNode<T> nuevo = new TreeNode<T>(value);
+            Children.Add(nuevo);
+            return nuevo;
         }
 
         public int Count()
         {
             //TODO #5: Return the total number of elements in this tree
-
-            return 0;
+            int cont = 1;
+            for(int i = 0; i < Children.Count();i++)
+            {
+                cont += Children.Get(i).Count();
+            }
+            return cont;
         }
 
         public int Height()
         {
             //TODO #6: Return the height of this tree
+            int maxAlt = 0;
+            if (Children.Count() == 0)
+            {
+                return 0;
+            }
+            for(int i = 0; i < Children.Count(); i++)
+            {
+                maxAlt = Math.Max(maxAlt, Children.Get(i).Height());
+            }
 
-            return 0;
+            return maxAlt + 1;
         }
 
         public void Remove(T value)
         {
             //TODO #7: Remove the child node that has Value=value. Apply recursively
+            Boolean enctr = false;
+            TreeNode<T> node;
+            for (int i = 0; i < Children.Count() && !enctr; i++)
+            {
+                node = Children.Get(i);
+                if (node.Value.Equals(value))
+                {
+                    Children.Remove(i);
+                    enctr = true;
+                }
+                else
+                {
+                    node.Remove(value);
+                }
+            }
+            
         }
 
         public TreeNode<T> Find(T value)
         {
             //TODO #8: Return the node that contains this value (it might be this node or a child). Apply recursively
+            if (Value.Equals(value))
+            {
+                return this;
+            }
+            else
+            {
+                for (int i = 0; i < Children.Count(); i++)
+                {
+                    TreeNode<T> child = Children.Get(i);
 
-            return null;
+
+                    if (child.Value.Equals(value))
+                    {
+                        return child;
+                    }
+
+
+                    TreeNode<T> resultado = child.Find(value);
+
+
+                    if (resultado != null)
+                    {
+                        return resultado;
+                    }
+                }
+                return null;
+            }
         }
 
         public void Remove(TreeNode<T> node)
         {
             //TODO #9: Same as #6, but this method is given the specific node to remove, not the value
+            Remove(node.Value);
         }
     }
 }
